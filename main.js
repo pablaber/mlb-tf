1a5f2271ac9a350e96ddd7e145ca6cad3b86cc76
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2020-01-12 17:15:17 -0500

    add elo map updating

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 'use strict';
 
+const moment = require('moment');
 require('dotenv').config();
 
 const configFactory = require('./lib/config/config-factory');
 const MySportsFeedsMlbClient = require('./lib/client/my-sports-feeds-mlb-client');
 const GameInfo = require('./lib/app/GameInfo');
+const EloMap = require('./lib/app/EloMap');
+
+const { MSF_DATE_FORMAT } = require('./lib/config/constants');
+
+const FROM_DATE = '20190328';
+const TO_DATE = '20190501';
 
 /**
  * The main function of the app
@@ -15,27 +22,25 @@ async function main() {
     apiKey: config.mySportsFeeds.apiKey,
     password: config.mySportsFeeds.password,
   });
+  const eloMap = new EloMap();
 
-  const DATE = '20190629';
+  const currentParsingDate = moment(FROM_DATE, MSF_DATE_FORMAT);
+  const endingDate = moment(TO_DATE, MSF_DATE_FORMAT);
 
-  const games = await client.getDailyTeamGameLogs({
-    date: DATE,
-  });
-  for (const gameLog of games.gamelogs) {
-    const gameInfo = new GameInfo();
-
-    const rawGameBoxscore = await client.getGameBoxscore({
-      date: DATE,
-      gameId: gameLog.game.id,
+  for (let date = currentParsingDate.format(MSF_DATE_FORMAT);
+    currentParsingDate.isBefore(endingDate);
+    date = currentParsingDate.add(1, 'day').format(MSF_DATE_FORMAT)) {
+    console.log(date);
+    const { games } = await client.getDailyTeamGameLogs({
+      date,
     });
-    gameInfo.loadBoxscore(rawGameBoxscore);
-
-    const rawGameLineup = await client.getGameLineup({
-      date: DATE,
-      gameId: gameLog.game.id,
-    });
-    gameInfo.loadLineup(rawGameLineup);
+    for (const gameObject of games) {
+      const gameInfo = await GameInfo.loadFromGameObject(client, gameObject);
+      eloMap.update(gameInfo);
+    }
   }
+
+  console.log(JSON.stringify(eloMap.eloMap));
 };
 
 (async () => {
